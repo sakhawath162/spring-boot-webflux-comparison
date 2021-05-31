aadb20e7a2cb1a2d514ed7eea35e2ce7e4e1dde7
Sakhawath Hossain <72663172+sakhawath162@users.noreply.github.com>
2021-05-31 15:14:33 +0600

    Delete Springwebflux-MongoDB directory

--- Springwebflux-MongoDB/src/main/java/com/example/SpringwebfluxMongoDB/Repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SpringwebfluxMongoDB.Repository;
-
-import com.example.SpringwebfluxMongoDB.Model.Student;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface StudentRepository extends ReactiveMongoRepository<Student,String> {
-}
